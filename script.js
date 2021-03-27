b35fc5383b605c671a0dbfee51af9cb369e010cc
IDMiller2020 <psiauto@gmail.com>
2021-03-26 23:38:25 -0600

    Basic button and info functionality completed

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let numOfWidgets = 0
 
 let widgetBuilder = {
-  model: {number: 100, upgradeCost: 50},
-  widgetsPerClick: 1,
-  numOfUpgrade2: 0,
-  numOfUpgrade3: 0,
-  numOfUpgrade4: 0
+  model: {number: 0, upgradeCost: 50, clickMod: 1},
+  power: {number: 0, upgradeCost: 100, clickMod: 10},
+  ai: {number: 0, upgradeCost: 2000, clickMod: 20},
+  quantum: {number: 0, upgradeCost: 10000, clickMod: 50},
+  widgetsPerClick: 1
 };
 
 function buildWidget() {
@@ -14,17 +14,33 @@ function buildWidget() {
 }
 
 function purchaseUpgrade(upgrade) {
-  if (upgrade === 'model' && numOfWidgets >= widgetBuilder.model.upgradeCost) {
-    numOfWidgets -= widgetBuilder.model.upgradeCost
-    widgetBuilder.model.number += 100
-    widgetBuilder.widgetsPerClick = widgetBuilder.model.number / 100
-    widgetBuilder.model.upgradeCost = widgetBuilder.model.upgradeCost + 50
-    document.getElementById('widgetCounter').innerText = `Widget Inventory: ${numOfWidgets} widgets`
-    document.getElementById('upgrade1Counter').innerText = `You are currently using ${(widgetBuilder.model.number / 100) - 1} model upgrades (+${(widgetBuilder.model.number / 100) - 1} production per click)`
-    document.getElementById('modelCounter').innerText = `Current Widget Maker Model: X${widgetBuilder.model.number}`
-    document.getElementById('modelUpgradeButton').innerText = `Upgrade to X${widgetBuilder.model.number + 100}: Cost = ${widgetBuilder.model.upgradeCost} Widgets`
-    
+  if (numOfWidgets >= widgetBuilder[upgrade].upgradeCost) {
+    numOfWidgets -= widgetBuilder[upgrade].upgradeCost
+    widgetBuilder[upgrade].number += 1
+    widgetBuilder.widgetsPerClick += widgetBuilder[upgrade].clickMod
+    widgetBuilder[upgrade].upgradeCost += widgetBuilder[upgrade].upgradeCost
   }
-
-
+  draw()
 }
+
+function draw() {
+  document.getElementById('widgetCounter').innerText = `Widget Inventory: ${numOfWidgets} widgets`
+
+  document.getElementById('modelUpgradeCounter').innerText = `+${(widgetBuilder.model.number * widgetBuilder.model.clickMod)} production for ${widgetBuilder.model.number} Model upgrades.`
+  document.getElementById('modelCounter').innerText = `Current Widget Maker Model: X${(widgetBuilder.model.number) * 100}`
+  document.getElementById('modelUpgradeButton').innerText = `Upgrade model: Cost = ${widgetBuilder.model.upgradeCost} Widgets`
+
+  document.getElementById('powerUpgradeCounter').innerText = `+${(widgetBuilder.power.number * widgetBuilder.power.clickMod)} production for ${widgetBuilder.power.number} Power upgrades.`
+  document.getElementById('powerModuleCounter').innerText = `Current number of Power Modules: ${widgetBuilder.power.number} Modules`
+  document.getElementById('powerUpgradeButton').innerText = `Upgrade power: Cost = ${widgetBuilder.power.upgradeCost} Widgets`
+
+  document.getElementById('machineAIUpgradeCounter').innerText = `+${(widgetBuilder.ai.number * widgetBuilder.ai.clickMod)} production for ${widgetBuilder.ai.number} Machine AI upgrades.`
+  document.getElementById('machineAICounter').innerText = `Current number of AI upgrades: ${widgetBuilder.ai.number} upgrades`
+  document.getElementById('machineAIUpgradeButton').innerText = `Upgrade AI: Cost = ${widgetBuilder.ai.upgradeCost} Widgets`
+
+  document.getElementById('quantumProcessorUpgradeCounter').innerText = `+${(widgetBuilder.quantum.number * widgetBuilder.quantum.clickMod)} production for ${widgetBuilder.quantum.number} Quantum Processor upgrades.`
+  document.getElementById('quantumProcessorCounter').innerText = `Current number of Quantum Processor upgrades: ${widgetBuilder.quantum.number} upgrades`
+  document.getElementById('quantumProcessorUpgradeButton').innerText = `Upgrade Quantum Processor: Cost = ${widgetBuilder.quantum.upgradeCost} Widgets`
+}
+
+draw()
